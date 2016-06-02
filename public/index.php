5d2872de2a3b4f9c9296e0698454be028f254791
taodutra <taodutra@gmail.com>
2016-06-02 09:40:55 -0300

    Menu, navegação com scroll, conteúdos e intro

--- public/index.php
+++ public/index.php
@@ -1,25 +1,117 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>VC 2016</title>
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+	<head>
+		<meta charset="UTF-8">
+		<title>VC 2016</title>
+		<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,900,300' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/app.css">
+	</head>
+	<body class="set-navbar-fixed-top-desktop">
+		<nav class="navbar navbar-fixed-top-desktop navbar-inverse" id="navbar">
+			<div class="container">
+				<h1><a href=""><img src="img/main-logo-white.png" alt=""></a></h1>
+				<ul>
+					<li><a href="#intro" rel="scroll">Intro</a></li>
+					<li><a href="#area-1" rel="scroll">Área 1</a></li>
+					<li><a href="#area-2" rel="scroll">Área 2</a></li>
+					<li><a href="#area-3" rel="scroll">Área 3</a></li>
+					<li><a href="#area-4" rel="scroll">Área 4</a></li>
+					<li><a href="#area-5" rel="scroll">Área 5</a></li>
+				</ul>
+			</div>
+		</nav>
+		<!-- .navbar -->
+		<section class="main-wrapper main">
+			<div class="container">
 
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/app.css">
-</head>
-<body>
-	
-</body>
+				<section class="area" id="intro">
+					<article class="graph full">
+						<picture>
+							<source srcset="img/intro.png" media="(min-width: 850px)">
+							<img srcset="img/intro-min.png" alt="">
+						</picture>
+					</article>
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/slick.min.js"></script>
-    <script src="js/picturefill.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+				</section>
+				<section class="area" id="area-1">
+					<article class="graph left">
+						It's a graphic!
+					</article>
+					<article class="text right">
+						<h2>Área 1</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis neque non sem semper ullamcorper. Phasellus rhoncus consequat justo non bibendum. In hac habitasse platea dictumst. Morbi condimentum pulvinar magna. Proin nibh velit, tincidunt vel lorem quis, pharetra dignissim nunc. Donec lacinia in urna eget vestibulum. Nam quis dui mauris. Aliquam quis auctor sapien. Quisque tincidunt lorem eu dapibus consectetur. Nullam erat sapien, malesuada nec justo non, blandit dapibus lorem. Phasellus ut dolor ac turpis commodo euismod. Integer sollicitudin sodales odio, eu malesuada lorem aliquet sit amet. Etiam pretium urna at odio elementum tristique. Nullam at nisi pellentesque, laoreet erat et, pharetra lacus. Curabitur ac feugiat ex.</p>
 
-    <script src="js/d3.v4.0.0-alpha.44.min.js"></script>
+						<p>Phasellus rhoncus consequat justo non bibendum. In hac habitasse platea dictumst. Morbi condimentum pulvinar magna. Proin nibh velit, tincidunt vel lorem quis, pharetra dignissim nunc. Donec lacinia in urna eget vestibulum. Nam quis dui mauris. Aliquam quis auctor sapien. Quisque tincidunt lorem eu dapibus consectetur. Nullam erat sapien, malesuada nec justo non, blandit dapibus lorem. Phasellus ut dolor ac turpis commodo euismod. Integer sollicitudin sodales odio, eu malesuada lorem aliquet sit amet. Etiam pretium urna at odio elementum tristique. Nullam at nisi pellentesque, laoreet erat et, pharetra lacus. Curabitur ac feugiat ex.</p>
+					</article>
+				</section>
+				<!-- #area-1 -->
 
-    <script src="js/app.js" type="text/javascript"></script>
+				<section class="area" id="area-2">
+					<article class="graph right">
+						It's a graphic!
+					</article>
+					<article class="text left">
+						<h2>Área 2</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis neque non sem semper ullamcorper. Phasellus rhoncus consequat justo non bibendum. In hac habitasse platea dictumst. Morbi condimentum pulvinar magna. Proin nibh velit, tincidunt vel lorem quis, pharetra dignissim nunc. Donec lacinia in urna eget vestibulum. Nam quis dui mauris. Aliquam quis auctor sapien. Quisque tincidunt lorem eu dapibus consectetur. Nullam erat sapien, malesuada nec justo non, blandit dapibus lorem. Phasellus ut dolor ac turpis commodo euismod. Integer sollicitudin sodales odio, eu malesuada lorem aliquet sit amet. Etiam pretium urna at odio elementum tristique. Nullam at nisi pellentesque, laoreet erat et, pharetra lacus. Curabitur ac feugiat ex.</p>
+
+						<p>Phasellus rhoncus consequat justo non bibendum. In hac habitasse platea dictumst. Morbi condimentum pulvinar magna. Proin nibh velit, tincidunt vel lorem quis, pharetra dignissim nunc. Donec lacinia in urna eget vestibulum. Nam quis dui mauris. Aliquam quis auctor sapien. Quisque tincidunt lorem eu dapibus consectetur. Nullam erat sapien, malesuada nec justo non, blandit dapibus lorem. Phasellus ut dolor ac turpis commodo euismod. Integer sollicitudin sodales odio, eu malesuada lorem aliquet sit amet. Etiam pretium urna at odio elementum tristique. Nullam at nisi pellentesque, laoreet erat et, pharetra lacus. Curabitur ac feugiat ex.</p>
+					</article>
+				</section>
+
+				<!-- #area-2 -->
 
 
+
+				<section class="area" id="area-3">
+					<article class="graph full">
+						It's a graphic!
+					</article>
+				</section>
+
+				<!-- #area-3 -->
+
+
+				<section class="area" id="area-4">
+					<article class="text full">
+						<h2>Área 4</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis neque non sem semper ullamcorper. Phasellus rhoncus consequat justo non bibendum. In hac habitasse platea dictumst. Morbi condimentum pulvinar magna. Proin nibh velit, tincidunt vel lorem quis, pharetra dignissim nunc. Donec lacinia in urna eget vestibulum. Nam quis dui mauris. Aliquam quis auctor sapien. Quisque tincidunt lorem eu dapibus consectetur. Nullam erat sapien, malesuada nec justo non, blandit dapibus lorem. Phasellus ut dolor ac turpis commodo euismod. Integer sollicitudin sodales odio, eu malesuada lorem aliquet sit amet. Etiam pretium urna at odio elementum tristique. Nullam at nisi pellentesque, laoreet erat et, pharetra lacus. Curabitur ac feugiat ex.</p>
+
+						<p>Phasellus rhoncus consequat justo non bibendum. In hac habitasse platea dictumst. Morbi condimentum pulvinar magna. Proin nibh velit, tincidunt vel lorem quis, pharetra dignissim nunc. Donec lacinia in urna eget vestibulum. Nam quis dui mauris. Aliquam quis auctor sapien. Quisque tincidunt lorem eu dapibus consectetur. Nullam erat sapien, malesuada nec justo non, blandit dapibus lorem. Phasellus ut dolor ac turpis commodo euismod. Integer sollicitudin sodales odio, eu malesuada lorem aliquet sit amet. Etiam pretium urna at odio elementum tristique. Nullam at nisi pellentesque, laoreet erat et, pharetra lacus. Curabitur ac feugiat ex.</p>
+					</article>
+				</section>
+
+				<!-- #area-4 -->
+
+				<section class="area" id="area-5">
+					<article class="graph left">
+						It's a graphic!
+					</article>
+					<article class="text right">
+						<h2>Área 5</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis neque non sem semper ullamcorper. Phasellus rhoncus consequat justo non bibendum. In hac habitasse platea dictumst. Morbi condimentum pulvinar magna. Proin nibh velit, tincidunt vel lorem quis, pharetra dignissim nunc. Donec lacinia in urna eget vestibulum. Nam quis dui mauris. Aliquam quis auctor sapien. Quisque tincidunt lorem eu dapibus consectetur. Nullam erat sapien, malesuada nec justo non, blandit dapibus lorem. Phasellus ut dolor ac turpis commodo euismod. Integer sollicitudin sodales odio, eu malesuada lorem aliquet sit amet. Etiam pretium urna at odio elementum tristique. Nullam at nisi pellentesque, laoreet erat et, pharetra lacus. Curabitur ac feugiat ex.</p>
+
+						<p>Phasellus rhoncus consequat justo non bibendum. In hac habitasse platea dictumst. Morbi condimentum pulvinar magna. Proin nibh velit, tincidunt vel lorem quis, pharetra dignissim nunc. Donec lacinia in urna eget vestibulum. Nam quis dui mauris. Aliquam quis auctor sapien. Quisque tincidunt lorem eu dapibus consectetur. Nullam erat sapien, malesuada nec justo non, blandit dapibus lorem. Phasellus ut dolor ac turpis commodo euismod. Integer sollicitudin sodales odio, eu malesuada lorem aliquet sit amet. Etiam pretium urna at odio elementum tristique. Nullam at nisi pellentesque, laoreet erat et, pharetra lacus. Curabitur ac feugiat ex.</p>
+					</article>
+				</section>
+				<!-- #area-5 -->
+
+			</div>
+		</section>
+		<!-- .main-wrapper -->
+		<footer class="footer">
+			<div class="container">
+				<p>Vert Inteligência Digital © 2016</p>
+			</div>
+		</footer>
+		<!-- .footer -->
+		
+	</body>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/slick.min.js"></script>
+	<script src="js/picturefill.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/d3.v4.0.0-alpha.44.min.js"></script>
+	<script src="js/app.js" type="text/javascript"></script>
 </html>
--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,48 @@ window.App = window.App || {
 
 		console.log('init!');
 
+		this.headerHeight = $('.navbar').height();
+
+		console.log(this.headerHeight);
+
+		this.setLinks();
+
+	},
+
+	setLinks: function(){
+
+		var that = this,
+			$scrollLink = $('a[rel=scroll]');
+
+		$scrollLink.on('click', function(e){
+			e.preventDefault();
+
+			$t = $(this), target = $t.attr('href'), offset = that.isMobileView() ? 0 : that.headerHeight;
+
+			that.scrollTo(target,offset);
+		});
+
+	},
+
+	scrollTo: function(target, offset){
+		target = target || '#';
+		offset = offset || 0;
+
+
+		var y = $(target).position().top - offset;
+
+		console.log(y);
+
+		$('html, body').stop().animate({
+	        scrollTop: y
+	    }, 500);
+
+		// $('html,body').animate({ scrollTop: y+'px' }, 500);
+
+	},
+
+	isMobileView: function(){
+		return window.innerWidth < 850 ? true : false;
 	}
 
 };
